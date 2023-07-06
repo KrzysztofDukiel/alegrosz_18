29e24b2df56f31822d35a02c3e409b85f48549a9
unknown <krdukiel@interia.pl>
2023-07-06 13:13:38 +0200

    to do again

---
+++ src/api/api.js
@@ -0,0 +1,15 @@
+export async function getProduct(id) {
+    const response = await fetch(`http://localhost:3000/products/${id}`, {});
+    return response.json();
+}
+export async function editProduct(id, data) {
+    const response = await fetch(`http://localhost:3000/products/${id}`, {
+        method: "put",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+
+    return response.json();
+}
